7b7ea3d3c8a5c5fb9a6d9bd5230b6c51e8ec33fd
Aishwarya Parab <aiparab@ebay.com>
2024-05-09 11:18:25 +0530

    Movix App development completed

---
+++ src/components/genres/Genres.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./style.scss";
+import { useSelector } from 'react-redux';
+
+const Genres = ({ data }) => {
+  const { genres } = useSelector((state) => state.home);
+
+  return (
+    <div className='genres'>
+        {data?.map((id) => {
+            if (!genres[id]?.name) return;
+            return <div key={id} className='genre'>
+                {genres[id]?.name}
+            </div>
+        })}
+    </div>
+  )
+}
+
+export default Genres
